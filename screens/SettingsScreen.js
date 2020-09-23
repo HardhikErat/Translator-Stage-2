07e718a925b5072c9eeecc0a69bb3acc62517969
HardhikErat <58391944+HardhikErat@users.noreply.github.com>
2020-09-23 16:23:35 +0530

    Add files via upload

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,12 @@
+import{ React, component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native-elements';
+
+export default class SettingsScreen extends Component{
+    render() {
+        return(
+            <View>
+                <Text>Settings Screen</Text>
+            </View>
+        )
+    }
+}
